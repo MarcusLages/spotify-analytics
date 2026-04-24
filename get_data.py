9ff2213facb196172e89dd975842c793750249bd
MarcusLages <marcusvlages@gmail.com>
2026-04-24 10:03:46 -0700

    Initial code snippet to test getting spotify data

---
+++ get_data.py
@@ -0,0 +1,19 @@
+import os
+import spotipy
+from dotenv import load_dotenv
+from spotipy.oauth2 import SpotifyOAuth
+
+load_dotenv()
+sp = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(
+        client_id=os.getenv("CLIENT_ID"),
+        client_secret=os.getenv("CLIENT_SECRET"),
+        redirect_uri=os.getenv("REDIRECT_URI"),
+        scope=os.getenv("SCOPE")
+    )
+)
+
+results = sp.current_user_saved_tracks()
+for idx, item in enumerate(results['items']):
+    track = item['track']
+    print(idx, track['artists'][0]['name'], " – ", track['name'])
