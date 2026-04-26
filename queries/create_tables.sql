2a5eb31148f1386f6aa2c51db50012584344fd2f
MarcusLages <marcusvlages@gmail.com>
2026-04-25 19:11:55 -0700

    Changed name conventions of constraints, size of IDs to 22 chars and small changes in numerical intervals and default values

--- queries/create_tables.sql
+++ queries/create_tables.sql
@@ -1,5 +1,5 @@
 CREATE TABLE Artists (
-    id   VARCHAR(62), -- PK
+    id   VARCHAR(22), -- PK
     name VARCHAR(255) NOT NULL,
     CONSTRAINT ArtistsPK PRIMARY KEY(id)
 );
@@ -8,44 +8,37 @@ CREATE TYPE ALBUMTYPE as ENUM('album', 'single', 'compilation');
 CREATE TYPE DATEPRECISION as ENUM('year', 'month', 'day');
 
 CREATE TABLE Albums (
-    id                     VARCHAR(62), -- PK
+    id                     VARCHAR(22), -- PK
     name                   VARCHAR(255) NOT NULL,
     type                   ALBUMTYPE DEFAULT 'album',
-    total_songs            SMALLINT,
     release_date           DATE,
     release_date_precision DATEPRECISION DEFAULT 'year',
-    added_at               DATE DEFAULT CURRENT_DATE(),
-    CONSTRAINT AlbumsPK PRIMARY KEY(id),
-    CONSTRAINT PositiveTotalSongs CHECK (total_songs > 0)
+    added_at               DATE DEFAULT CURRENT_DATE,
+    CONSTRAINT pk_albums_id PRIMARY KEY(id)
 );
 
 CREATE TABLE Songs (
-    id          VARCHAR(62), -- PK
+    id          VARCHAR(22), -- PK
     name        VARCHAR(255) NOT NULL,
-    album_id    VARCHAR(62) NOT NULL,
-    track_num   SMALLINT DEFAULT 0,
+    album_id    VARCHAR(22) NOT NULL,
+    track_num   SMALLINT DEFAULT 1,
     duration_ms INTEGER,
-    is_playable BOOLEAN,
-    added_at    DATE DEFAULT CURRENT_DATE(),
-    CONSTRAINT SongsPK PRIMARY KEY(id),
-    CONSTRAINT SongsAlbumFK FOREIGN KEY(album_id) REFERENCES Albums(id),
-    CONSTRAINT PositiveTrackNum CHECK (track_num > 0),
-    CONSTRAINT PositiveDurationMs CHECK (duration_ms > 0)
-);
-
--- Relation Tables
-CREATE TABLE ArtistsInAlbums (
-    artist_id VARCHAR(62),
-    album_id  VARCHAR(62),
-    CONSTRAINT ArtistAlbumPK PRIMARY KEY (artist_id, album_id),
-    CONSTRAINT ArtistFK FOREIGN KEY(artist_id) REFERENCES Artists(id),
-    CONSTRAINT AlbumFK FOREIGN KEY(album_id) REFERENCES Albums(id)
+    is_playable BOOLEAN DEFAULT NULL,
+    popularity  SMALLINT,
+    added_at    DATE DEFAULT CURRENT_DATE,
+    CONSTRAINT pk_songs_id          PRIMARY KEY(id),
+    CONSTRAINT fk_songs_album_id    FOREIGN KEY(album_id) REFERENCES Albums(id),
+    CONSTRAINT ck_songs_track_num   CHECK (track_num > 0),
+    CONSTRAINT ck_songs_duration_ms CHECK (duration_ms > 0),
+    CONSTRAINT ck_songs_popularity  CHECK (popularity >= 0 AND popularity <= 100)
 );
 
 CREATE TABLE ArtistsInSongs (
-    artist_id VARCHAR(62),
-    song_id  VARCHAR(62),
-    CONSTRAINT ArtistSongPK PRIMARY KEY (artist_id, song_id),
-    CONSTRAINT ArtistFK FOREIGN KEY(artist_id) REFERENCES Artists(id),
-    CONSTRAINT SongFK FOREIGN KEY(song_id) REFERENCES Songs(id)
+    artist_id    VARCHAR(22),
+    song_id      VARCHAR(22),
+    artist_order SMALLINT DEFAULT 0,
+    CONSTRAINT pk_artistsinsongs_artist_id_song_id PRIMARY KEY (artist_id, song_id),
+    CONSTRAINT fk_artistsinsongs_artist_id         FOREIGN KEY(artist_id) REFERENCES Artists(id) ON DELETE CASCADE,
+    CONSTRAINT fk_artistsinsongs_song_id           FOREIGN KEY(song_id) REFERENCES Songs(id) ON DELETE CASCADE,
+    CONSTRAINT ck_artistsinsongs_artist_order      CHECK(artist_order >= 0)
 );
