152a9857dd2306e9471b1abca3433fe72476253e
MarcusLages <marcusvlages@gmail.com>
2026-04-26 21:33:50 -0700

    Created functions to delete a song if the artist was deleted and no other artist is part of that song

--- queries/triggers.sql
+++ queries/triggers.sql
@@ -1,6 +1,7 @@
+--* Trigger to set updated_at automatically
 CREATE OR REPLACE FUNCTION fn_set_updated_at()
     RETURNS TRIGGER AS $updated_at$
-    BEGIN;
+    BEGIN
         NEW.updated_at = CURRENT_DATE;
         RETURN NEW;
     END;
@@ -20,3 +21,24 @@ CREATE OR REPLACE TRIGGER trg_songs_updated_at
     BEFORE UPDATE ON Songs
     FOR EACH ROW
     EXECUTE FUNCTION fn_set_updated_at();
+
+--* Trigger to delete all songs of an artist, when the artist is deleted,
+--* whenever the artist is the only one in that song
+CREATE OR REPLACE FUNCTION fn_song_deleted_when_artist_deleted()
+    RETURNS TRIGGER AS $songs_deleted_when_artist_deleted$
+    BEGIN
+        DELETE FROM Songs
+        WHERE id IN (
+            SELECT ais.song_id
+            FROM ArtistsInSongs ais
+            WHERE ais.artist_id = OLD.id
+                AND NOT EXISTS (
+                    SELECT 1
+                    FROM ArtistsInSongs ais2
+                    WHERE ais2.artist_id <> OLD.id
+                        AND ais2.song_id = ais.song_id
+                )
+        );
+        RETURN OLD;
+    END;
+$songs_deleted_when_artist_deleted$ LANGUAGE PLPGSQL
