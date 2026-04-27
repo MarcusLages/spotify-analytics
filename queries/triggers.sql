a8d5323155338890b229e18d5c9283d28d90c279
MarcusLages <marcusvlages@gmail.com>
2026-04-26 21:53:41 -0700

    Added trigger to delete an artist if the artist has no remaining songs (AIS and Songs)

--- queries/triggers.sql
+++ queries/triggers.sql
@@ -7,17 +7,17 @@ CREATE OR REPLACE FUNCTION fn_set_updated_at()
     END;
 $updated_at$ LANGUAGE PLPGSQL;
 
-CREATE OR REPLACE TRIGGER trg_artists_updated_at
+CREATE OR REPLACE TRIGGER trg_artists_updated_at_update
     BEFORE UPDATE ON Artists
     FOR EACH ROW
     EXECUTE FUNCTION fn_set_updated_at();
 
-CREATE OR REPLACE TRIGGER trg_albums_updated_at
+CREATE OR REPLACE TRIGGER trg_albums_updated_at_update
     BEFORE UPDATE ON Albums
     FOR EACH ROW
     EXECUTE FUNCTION fn_set_updated_at();
 
-CREATE OR REPLACE TRIGGER trg_songs_updated_at
+CREATE OR REPLACE TRIGGER trg_songs_updated_at_update
     BEFORE UPDATE ON Songs
     FOR EACH ROW
     EXECUTE FUNCTION fn_set_updated_at();
@@ -41,4 +41,61 @@ CREATE OR REPLACE FUNCTION fn_song_deleted_when_artist_deleted()
         );
         RETURN OLD;
     END;
-$songs_deleted_when_artist_deleted$ LANGUAGE PLPGSQL
+$songs_deleted_when_artist_deleted$ LANGUAGE PLPGSQL;
+
+CREATE OR REPLACE TRIGGER trg_songs_artist_delete
+    BEFORE DELETE ON Artists
+    FOR EACH ROW
+    EXECUTE FUNCTION fn_song_deleted_when_artist_deleted();
+
+--* Trigger to delete an artist if the artist has no remaining songs
+CREATE OR REPLACE FUNCTION fn_artist_delete_if_no_songs()
+    RETURNS TRIGGER AS $artist_delete_if_no_songs$
+    BEGIN
+        DELETE FROM Artists
+        WHERE id IN (
+            SELECT ais.artist_id
+            FROM ArtistsInSongs ais
+            WHERE ais.song_id = OLD.id
+                AND NOT EXISTS (
+                    SELECT 1
+                    FROM ArtistsInSongs ais2
+                    WHERE ais.artist_id = ais2.artist_id
+                        AND ais2.song_id <> OLD.id
+                );
+        );
+        RETURN OLD;
+    END;
+$artist_delete_if_no_songs$ LANGUAGE PLPGSQL;
+
+CREATE OR REPLACE TRIGGER trg_artists_songs_delete
+    BEFORE DELETE ON Songs
+    FOR EACH ROW
+    EXECUTE FUNCTION fn_artist_delete_if_no_songs();
+
+--* Trigger to delete an artist if the artist has no remaining songs
+--* (now checking for DELETEs and UPDATEs in the ArtistsInSongs)
+CREATE OR REPLACE FUNCTION fn_artistsinsongs_delete_artist_if_no_songs()
+    RETURN TRIGGER AS $artistsinsongs_delete_artist_if_no_songs$
+    BEGIN
+        DELETE FROM Artists
+        WHERE id = OLD.artist_id
+            AND NOT EXISTS (
+                SELECT 1
+                FROM ArtistsInSongs ais
+                WHERE ais.artist_id = OLD.artist_id
+                    AND ais.song_id <> OLD.song_id
+            );
+        RETURN OLD;
+    END;
+$artistsinsongs_delete_artist_if_no_songs$ LANGUAGE PLPGSQL;
+
+CREATE OR REPLACE TRIGGER trg_artistsinsongs_delete_artists_no_songs_delete
+    BEFORE DELETE ON ArtistsInSongs
+    FOR EACH ROW
+    EXECUTE FUNCTION fn_artistsinsongs_delete_artist_if_no_songs();
+
+CREATE OR REPLACE TRIGGER trg_artistsinsongs_delete_artists_no_songs_update
+    BEFORE UPDATE ON ArtistsInSongs
+    FOR EACH ROW
+    EXECUTE FUNCTION fn_artistsinsongs_delete_artist_if_no_songs();
